833e66619cda9ae6dbeacf062cf8fc20fe9f15c3
sami2410c <sami2410c@aptechgdn.net>
2025-04-22 23:12:14 +0500

    change

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,27 @@
+function demo() {
+    const email = document.getElementById("email").value;
+    alert(email);
+}
 
+
+
+
+  let cards = document.querySelectorAll('.break-fast-inner,.contact-1, .contact-2');
+
+  function animateCardsOnScroll() {
+    cards.forEach(card => {
+      const cardTop = card.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (cardTop < windowHeight - 50) {
+        card.classList.add('visible');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', animateCardsOnScroll);
+  window.addEventListener('load', animateCardsOnScroll);
+
+
+
+  
